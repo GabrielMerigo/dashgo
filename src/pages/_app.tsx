069b3ce8a9c0540462406cab15157efa5a2707da
Gabriel Merigo <gabriel.merigo.dev@gmail.com>
2021-09-23 22:02:29 -0300

    Ajustando fontes e cores

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,13 @@
 import { AppProps } from 'next/app';
+import { ChakraProvider } from '@chakra-ui/react';
+import { theme } from '../styles/theme';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  <h1>DashGo</h1>
+  return (
+    <ChakraProvider theme={theme}>
+        <Component {...pageProps} />
+    </ChakraProvider>
+  )
 }
 
 export default MyApp
