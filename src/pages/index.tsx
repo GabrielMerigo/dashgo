bd00c148588618d56204f628fffaf061b41d45e5
Gabriel Merigo <gabriel.merigo.dev@gmail.com>
2021-09-27 22:18:17 -0300

    More accs

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { Flex, Input } from '@chakra-ui/react';
+import { Flex, Input, Button } from '@chakra-ui/react';
 
 export default function Home() {
   return (
@@ -15,9 +15,15 @@ export default function Home() {
         bg="gray.800"
         p="8"
         borderRadius={8}
-
+        flexDir="column"
       >
-        <Input name="email" type="email" >
+        <Input
+          name="email"
+          type="email"
+        />
+        <Input name="password" type="password" />
+
+        <Button type="submit" mt="6" colorScheme="pink">Entrar</Button>
       </Flex>
     </Flex>
   )
