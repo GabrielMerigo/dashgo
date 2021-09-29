a083cc263d448755e93eaf43546cb735b21d7671
Gabriel Merigo <gabriel.merigo.dev@gmail.com>
2021-09-28 23:57:16 -0300

    More accs

---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+import { Flex } from '@chakra-ui/react';
+
+export default function Header() {
+  return (
+    <Flex
+      as="header"
+      w="100%"
+      maxWidth={1480}
+    >
+
+    </Flex>
+  )
+}
