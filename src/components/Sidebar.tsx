0763c95db506da201044ed23c683f5af8478ce51
Gabriel Merigo <gabriel.merigo.dev@gmail.com>
2021-09-29 01:06:54 -0300

    Update Sidebar.tsx

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,5 +1,5 @@
 import { Box, Stack, Text, Link, Icon } from '@chakra-ui/react';
-import { RiDashboardLine } from 'react-icons/ri';
+import { RiDashboardLine, RiContactsLine, RiInputMethodLine, RiGitMergeLine } from 'react-icons/ri';
 
 export default function Sidebar() {
   return (
@@ -12,8 +12,27 @@ export default function Sidebar() {
               <Icon as={RiDashboardLine} fontSize="20"/>
               <Text ml="4" fontWeight="medium">Dashboard</Text>
             </Link>
+            <Link display="flex" align="center" >
+              <Icon as={RiContactsLine} fontSize="20"/>
+              <Text ml="4" fontWeight="medium">Usuários</Text>
+            </Link>
           </Stack>
         </Box>
+
+        <Box>
+          <Text fontWeight="bold" color="gray.400" fontSize="small">AUTOMAÇÃO</Text>
+          <Stack spacing="4" mt="8" align="stretch">
+            <Link display="flex" align="center" >
+              <Icon as={RiInputMethodLine} fontSize="20"/>
+              <Text ml="4" fontWeight="medium">Formulário</Text>
+            </Link>
+            <Link display="flex" align="center" >
+              <Icon as={RiGitMergeLine} fontSize="20"/>
+              <Text ml="4" fontWeight="medium">Automação</Text>
+            </Link>
+          </Stack>
+        </Box>
+
       </Stack>
     </Box>
   )
