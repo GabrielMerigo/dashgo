abf18056fe1b80e794321cd7ef4aeaab8d89cf9d
Gabriel Merigo <gabriel.merigo.dev@gmail.com>
2021-09-30 00:07:23 -0300

    Update dashboard.tsx

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,21 +1,64 @@
-import { Flex, SimpleGrid, Box, Text } from "@chakra-ui/react";
+import { Flex, SimpleGrid, Box, Text, theme } from "@chakra-ui/react";
 import Header from "../components/Header";
 import Sidebar from "../components/Sidebar";
-import dynamic from 'next/dynamic' ;
+import dynamic from 'next/dynamic';
 
 const Chart = dynamic(() => import('react-apexcharts'), {
   ssr: false
 });
 
-const option = {
-
+const options = {
+  chart: {
+    toolbar: {
+      show: false
+    },
+    zoom: {
+      enabled: false
+    },
+    foreColor: theme.colors.gray[500]
+  },
+  grid: {
+    show: false
+  },
+  dataLabels: {
+    enabled: false
+  },
+  tooltip: {
+    enabled: false
+  },
+  xaxis: {
+    type: 'datetime',
+    axisBorder: {
+      color: theme.colors.gray[600]
+    },
+    axisTicks: {
+      color: theme.colors.gray[600]
+    },
+    categories: [
+      '2021-03-18T00:00:00.000Z',
+      '2021-03-19T00:00:00.000Z',
+      '2021-03-20T00:00:00.000Z',
+      '2021-03-21T00:00:00.000Z',
+      '2021-03-22T00:00:00.000Z',
+      '2021-03-23T00:00:00.000Z',
+    ],
+  },
+  fill: {
+    opacity: 0.3,
+    type: 'gradient',
+    gradient: {
+      shade: 'dark',
+      opacityFrom: 0.7,
+      opacityTo: 0.7,
+    }
+  }
 };
 
 const series = [
-  {name: 'series1', data: [21, 233, 12, 123, 76, 45]}
+  { name: 'series1', data: [21, 233, 12, 123, 76, 45] }
 ];
 
-export default function Dashboard(){
+export default function Dashboard() {
   return (
     <Flex direction="column" h="100vh">
       <Header />
@@ -30,7 +73,7 @@ export default function Dashboard(){
             borderRadius={8}
           >
             <Text fontSize="lg" mg="4">Inscritos da Semana</Text>
-            <Chart options={option} series={series} type="area" height={160} />
+            <Chart options={options} series={series} type="area" height={160} />
           </Box>
           <Box
             p="8"
@@ -38,6 +81,7 @@ export default function Dashboard(){
             borderRadius={8}
           >
             <Text fontSize="lg" mg="4">Taxa de Abetura</Text>
+            <Chart options={options} series={series} type="area" height={160} />
           </Box>
         </SimpleGrid>
       </Flex>
