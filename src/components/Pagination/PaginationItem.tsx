b6c1f1c0c8dd0afb2b008bab0c83ead5b5eaa0f5
Gabriel Merigo <gabriel.merigo.dev@gmail.com>
2021-10-05 23:56:38 -0300

    Pagination concluída

---
+++ src/components/Pagination/PaginationItem.tsx
@@ -0,0 +1,42 @@
+import { Button } from '@chakra-ui/button';
+
+interface PaginationProps {
+  number: number;
+  isCurrent?: boolean;
+}
+
+export function PaginationItem({
+  isCurrent = false,
+  number
+}: PaginationProps) {
+  if (isCurrent) {
+    return (
+      <Button
+        font="sm"
+        fontSize="xs"
+        width="4"
+        colorScheme="pink"
+        disabled
+        _disabled={{
+          bgColor: 'pink.500',
+          cursor: 'default',
+        }}
+      >
+        {number}
+      </Button>
+    )
+  }
+
+  return (
+    <Button
+      font="sm"
+      fontSize="xs"
+      width="4"
+      bgColor="gray.700"
+      _hover={{
+        bg: 'gray.500',
+      }}>
+      {number}
+    </Button>
+  )
+}
