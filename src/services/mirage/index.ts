bf5076594a96c8d7abd418ea1c008f921ac5c009
Gabriel Merigo <gabriel.merigo.dev@gmail.com>
2021-10-12 21:07:57 -0300

    Update index.ts

--- src/services/mirage/index.ts
+++ src/services/mirage/index.ts
@@ -13,8 +13,14 @@ export function makeServer(){
     },
 
     routes(){
+      this.namespace = 'api';
+      this.timing = 750;
+      
+      
       this.get('/users');
       this.post('/users');
+
+      this.namespace = '';
     }
   })
 
