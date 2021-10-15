32c343dadd2b355f3c301d373b3a76738f18b6df
Gabriel Merigo <gabriel.merigo.dev@gmail.com>
2021-10-15 00:13:22 -0300

    adicionado axios

---
+++ src/services/hooks/useUsers.ts
@@ -0,0 +1,33 @@
+import { useQuery } from "react-query";
+import { api } from "../api";
+
+type Users = {
+  createdAt: string;
+  email: string;
+  id: string;
+  name: string;
+}
+
+export function useUsers(){
+  return useQuery('users', async () => {
+    const { data } = await api.get('users');
+    const usersArray: Array<Users> = data.users;
+                                                                                                       
+    const users = usersArray.map(user => {
+      return {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+        createdAt: new Date(user.createdAt).toLocaleDateString('pt-BR', {
+          day: '2-digit',
+          month: 'long', 
+          year: 'numeric'
+        })
+      }
+    });
+
+    return users
+  }, {
+    staleTime: 1000 * 5 // 5 seconds
+  });
+}
