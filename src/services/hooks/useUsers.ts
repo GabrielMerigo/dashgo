18b76990637d3120117fe39941a54531f86acfd7
Gabriel Merigo <gabriel.merigo.dev@gmail.com>
2021-10-15 00:17:11 -0300

    Update useUsers.ts

--- src/services/hooks/useUsers.ts
+++ src/services/hooks/useUsers.ts
@@ -8,26 +8,28 @@ type Users = {
   name: string;
 }
 
-export function useUsers(){
-  return useQuery('users', async () => {
-    const { data } = await api.get('users');
-    const usersArray: Array<Users> = data.users;
-                                                                                                       
-    const users = usersArray.map(user => {
-      return {
-        id: user.id,
-        name: user.name,
-        email: user.email,
-        createdAt: new Date(user.createdAt).toLocaleDateString('pt-BR', {
-          day: '2-digit',
-          month: 'long', 
-          year: 'numeric'
-        })
-      }
-    });
+async function getUsers() {
+  const { data } = await api.get('users');
+  const usersArray: Array<Users> = data.users;
+                                                                                                     
+  const users = usersArray.map(user => {
+    return {
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      createdAt: new Date(user.createdAt).toLocaleDateString('pt-BR', {
+        day: '2-digit',
+        month: 'long', 
+        year: 'numeric'
+      })
+    }
+  });
 
-    return users
-  }, {
+  return users
+}
+
+export function useUsers(){
+  return useQuery('users', getUsers, {
     staleTime: 1000 * 5 // 5 seconds
   });
 }
